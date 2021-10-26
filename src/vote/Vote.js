2c9dda223ef74eb28bfe3cdb5b5a76b432d2b616
Ivan Schuetz <ivanschuetz@gmail.com>
2021-10-26 17:48:04 +0200

    Show empty votes view if no funds requested yet

--- src/vote/Vote.js
+++ src/vote/Vote.js
@@ -5,8 +5,19 @@ import { WithdrawalRequest } from "../WithdrawalRequest";
 
 export const Vote = (props) => {
   const [project, setProject] = useState([]);
-  const [withdrawalRequests, setWithdrawalRequests] = useState([]);
+  const [withdrawalRequests, setWithdrawalRequests] = useState(null);
   const [chainInvestmentData, setChainInvestmentData] = useState(null);
+
+  useEffect(() => {
+    console.log("loading project id: " + JSON.stringify(props.match.params));
+    init(
+      props.match.params.id,
+      props.myAddress,
+      setProject,
+      setWithdrawalRequests,
+      setChainInvestmentData
+    );
+  }, [props.match.params, props.myAddress]);
 
   const userDataView = () => {
     if (chainInvestmentData) {
@@ -21,16 +32,39 @@ export const Vote = (props) => {
     }
   };
 
-  useEffect(() => {
-    console.log("loading project id: " + JSON.stringify(props.match.params));
-    init(
-      props.match.params.id,
-      props.myAddress,
-      setProject,
-      setWithdrawalRequests,
-      setChainInvestmentData
-    );
-  }, [props.match.params, props.myAddress]);
+  const requestsView = () => {
+    if (Array.isArray(withdrawalRequests)) {
+      if (withdrawalRequests.length > 0) {
+        return withdrawalRequests.map((req) => (
+          <WithdrawalRequest
+            req={req.req}
+            buttonDisabled={
+              () =>
+                !props.myAddress || // not connected
+                !chainInvestmentData || // user data not loaded yet
+                !chainInvestmentData.investor_percentage || // doesn't have shares
+                req.user_voted // already voted
+            }
+            onButtonClick={async () => {
+              await vote(
+                props.myAddress,
+                props.showProgress,
+                props.statusMsg,
+                props.setMyBalance,
+                props.match.params.id,
+                req.req
+              );
+            }}
+            buttonLabel={"Vote"}
+          />
+        ));
+      } else {
+        return <div>{"The project owner hasn't requested any funds yet."}</div>;
+      }
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div>
@@ -38,32 +72,7 @@ export const Vote = (props) => {
         <div>
           <ProjectName project={project} />
           {userDataView()}
-          <div className="withdrawal-cell-container">
-            {withdrawalRequests &&
-              withdrawalRequests.map((req) => (
-                <WithdrawalRequest
-                  req={req.req}
-                  buttonDisabled={
-                    () =>
-                      !props.myAddress || // not connected
-                      !chainInvestmentData || // user data not loaded yet
-                      !chainInvestmentData.investor_percentage || // doesn't have shares
-                      req.user_voted // already voted
-                  }
-                  onButtonClick={async () => {
-                    await vote(
-                      props.myAddress,
-                      props.showProgress,
-                      props.statusMsg,
-                      props.setMyBalance,
-                      props.match.params.id,
-                      req.req
-                    );
-                  }}
-                  buttonLabel={"Vote"}
-                />
-              ))}
-          </div>
+          <div className="withdrawal-cell-container">{requestsView()}</div>
         </div>
       </div>
     </div>
