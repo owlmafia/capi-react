f8d2c6d9c7fc318ef04a030a6b4f98a08771b0c3
Ivan Schuetz <ivanschuetz@gmail.com>
2022-01-21 21:46:44 +0100

    Configure scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { CreateProject } from "./create/CreateProject";
 import { Invest } from "./invest/Invest";
