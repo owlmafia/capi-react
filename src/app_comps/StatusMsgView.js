0008020eb857544af6759f8d4af36fda47d52f35
Ivan Schuetz <ivanschuetz@gmail.com>
2022-02-08 18:17:15 +0100

    Handle success notifications like error notifications

--- src/app_comps/StatusMsgView.js
+++ src/app_comps/StatusMsgView.js
@@ -7,24 +7,27 @@ export const StatusMsgView = ({ statusMsgUpdater, statusMsg }) => {
     shortMsg = shortMsg.substring(0, maxMsgLength) + "...";
   }
 
+  var className;
   if (statusMsg.type === "success") {
-    return <div className="msg msg__success">{statusMsg.displayMsg}</div>;
+    className = "msg__success";
   } else if (statusMsg.type === "error") {
-    return (
-      <div className="msg msg__error">
-        <CopyPasteText text={shortMsg} copyText={statusMsg.copyMsg} />
-
-        <button className="msg__close" onClick={() => statusMsgUpdater.clear()}>
-          <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0.757324 0.757324L9.24261 9.24261" />
-            <path d="M0.757395 9.24261L9.24268 0.757324" />
-          </svg>
-        </button>
-      </div>
-    );
+    className = "msg__error";
   } else {
     throw Error("Invalid status msg type: " + statusMsg.type);
   }
+
+  return (
+    <div className={"msg " + className}>
+      <CopyPasteText text={shortMsg} copyText={statusMsg.copyMsg} />
+
+      <button className="msg__close" onClick={() => statusMsgUpdater.clear()}>
+        <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.757324 0.757324L9.24261 9.24261" />
+          <path d="M0.757395 9.24261L9.24268 0.757324" />
+        </svg>
+      </button>
+    </div>
+  );
 };
 
 export default StatusMsgView;
