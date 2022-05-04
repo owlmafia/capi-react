1f7b4b17280c53dd20c01c3e58725b31a808200f
Ivan Schuetz <ivanschuetz@gmail.com>
2022-05-04 08:54:22 +0200

    Update income vs. spending chart

--- src/charts/IncomeVsSpendingChart.js
+++ src/charts/IncomeVsSpendingChart.js
@@ -5,31 +5,23 @@ import renderBarChart from "./renderBarChart";
 export const IncomeVsSpendingChart = ({ chartData }) => {
   const chart = useRef(null);
 
-  const lineColors = useMemo(() => {
-    // HACK: TODO: derive correctly colors / legends from data. Currently e.g. order and count of lines is assumed when defining legends.
-    return ["#377eb8", "#e41a1c"];
+  const colors = useMemo(() => {
+    return ["#6BB9BC", "#DF5C60"];
   }, []);
 
   useEffect(() => {
     if (chartData && chart.current) {
-      renderBarChart(
-        chart.current,
-        chartData.flat_data_points,
-        chartData.chart_lines,
-        lineColors
-      );
+      renderBarChart(chart.current, chartData.points, colors);
     }
-    // for now no deps - mutable value doesn't cause a re-render
-    //   }, [chartData, lineColors]);
-  });
+  }, [chartData]);
 
   return (
     <div className="chart_with_legends_container">
       <svg width={200} height={200} ref={chart} />
       <ChartLegends
         legends={[
-          { color: lineColors[1], text: "Income" },
-          { color: lineColors[0], text: "Spending" },
+          { color: colors[1], text: "Income" },
+          { color: colors[0], text: "Spending" },
         ]}
       />
     </div>
