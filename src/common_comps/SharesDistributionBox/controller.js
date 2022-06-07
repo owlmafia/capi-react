878b0f4611a6d661cea6e3cbaf386c6acb0afdd8
Ivan Schuetz <ivanschuetz@gmail.com>
2022-06-07 12:14:06 +0200

    simulate no investors

--- src/common_comps/SharesDistributionBox/controller.js
+++ src/common_comps/SharesDistributionBox/controller.js
@@ -10,11 +10,25 @@ export const fetchSharesDistribution = async (
 ) => {
   try {
     const { bridge_shares_distribution } = await wasmPromise;
-    let res = await bridge_shares_distribution({
-      asset_id: assetId,
-      share_supply: assetSupply,
-      app_id: appId,
-    });
+    // let res = await bridge_shares_distribution({
+    //   asset_id: assetId,
+    //   share_supply: assetSupply,
+    //   app_id: appId,
+    // });
+    let res = {
+      holders: [
+        {
+          address: "",
+          label: "Not owned",
+          address_browser_link: "",
+          amount: "100",
+          percentage_formatted: "100 %",
+          percentage_number: "1",
+          type_: "not_owned",
+        },
+      ],
+      not_owned_shares: "100",
+    };
     console.log("Shares distribution res: " + JSON.stringify(res));
     setSharesDistr(res.holders);
     setNotOwnedShares(res.not_owned_shares);
