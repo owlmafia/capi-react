3a42dca51dabd51849f9c47acf25f5c2e02a410b
Nazarii Hunkevych <nazariyromanovich@gmail.com>
2022-07-05 13:16:35 +0300

    Style Max funding target input

--- src/wallet/SelectWallet.js
+++ src/wallet/SelectWallet.js
@@ -5,34 +5,32 @@ import { createWcWallet } from "./walletConnectWallet";
 export const SelectWallet = ({ deps, onConnected }) => {
   return (
     <div>
-      <div>
-        <SubmitButton
-          label={"Wallet Connect"}
-          className="button-primary full-width-btn mb-4"
-          onClick={async () =>
-            selectWallet(
-              deps,
-              createWcWallet(
-                deps.statusMsg,
-                deps.setMyAddress,
-                deps.setWcShowOpenWalletModal
-              ),
-              onConnected
-            )
-          }
-        />
-        <SubmitButton
-          label={"My Algo"}
-          className="button-primary full-width-btn"
-          onClick={async () =>
-            selectWallet(
-              deps,
-              createMyAlgoWallet(deps.statusMsg, deps.setMyAddress),
-              onConnected
-            )
-          }
-        />
-      </div>
+      <SubmitButton
+        label={"Wallet Connect"}
+        className="button-primary full-width-btn mb-4"
+        onClick={async () =>
+          selectWallet(
+            deps,
+            createWcWallet(
+              deps.statusMsg,
+              deps.setMyAddress,
+              deps.setWcShowOpenWalletModal
+            ),
+            onConnected
+          )
+        }
+      />
+      <SubmitButton
+        label={"My Algo"}
+        className="button-primary full-width-btn"
+        onClick={async () =>
+          selectWallet(
+            deps,
+            createMyAlgoWallet(deps.statusMsg, deps.setMyAddress),
+            onConnected
+          )
+        }
+      />
     </div>
   );
 };
