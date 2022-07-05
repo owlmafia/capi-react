25c715fc662bfc4804eb3737d2748ed8f357c5b4
Nazarii Hunkevych <nazariyromanovich@gmail.com>
2022-07-05 11:44:54 +0300

    Fix formatting

--- src/app_comps/StatusMsgUpdater.js
+++ src/app_comps/StatusMsgUpdater.js
@@ -1,17 +1,17 @@
-import { toFriendlyError } from './friendlyErrors'
-import { toast } from 'react-toastify'
+import { toFriendlyError } from "./friendlyErrors";
+import { toast } from "react-toastify";
 
 export const StatusMsgUpdater = (setStatusMsg) => ({
   // Display text as a success notification
   success(msg, hideClose) {
-    msg = msg + ''
-    console.log(msg)
+    msg = msg + "";
+    console.log(msg);
     toast(msg, {
       toastId: msg,
       type: toast.TYPE.SUCCESS,
       closeOnClick: false,
       position: toast.POSITION.TOP_CENTER,
-    })
+    });
   },
 
   // Displays text as error notification,
@@ -20,18 +20,18 @@ export const StatusMsgUpdater = (setStatusMsg) => ({
   // if friendly error, the copy paste text corresponds to the original (not friendly) text
   // if not friendly error, the copy paste text is equal to the displayed text
   error(msg, hideClose) {
-    msg = msg + ''
-    var displayMsg = msg
+    msg = msg + "";
+    var displayMsg = msg;
     try {
-      const friendlyMsg = toFriendlyError(msg)
+      const friendlyMsg = toFriendlyError(msg);
       if (friendlyMsg) {
-        msg = friendlyMsg + '\nOriginal error: ' + msg
-        displayMsg = friendlyMsg
+        msg = friendlyMsg + "\nOriginal error: " + msg;
+        displayMsg = friendlyMsg;
       }
     } catch (e) {
-      msg += '\n+Error mapping to friendly error: ' + (e + '')
+      msg += "\n+Error mapping to friendly error: " + (e + "");
     }
-    console.error('Error notification: %o', msg)
+    console.error("Error notification: %o", msg);
     // NOTE that for now msg (which contains the full original error message) isn't included in the notification
     // if user wants to send a report, they've to copy paste from the console
     toast(displayMsg, {
@@ -39,10 +39,10 @@ export const StatusMsgUpdater = (setStatusMsg) => ({
       type: toast.TYPE.ERROR,
       closeOnClick: false,
       position: toast.POSITION.TOP_CENTER,
-    })
+    });
   },
 
   clear() {
-    toast.dismiss()
+    toast.dismiss();
   },
-})
+});
