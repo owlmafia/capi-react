b1d108100819e4fa1689029a0476f1a4a2159159
Ivan Schuetz <ivanschuetz@gmail.com>
2022-07-16 19:07:09 +0200

    Workaround for source map not found erros after CRA 5 upgrade
    https://github.com/facebook/create-react-app/pull/11752#issuecomment-1137238078

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const { override } = require("customize-cra");
 
 module.exports = function override(config, env) {
   const wasmExtensionRegExp = /\.wasm$/;
@@ -43,6 +44,25 @@ module.exports = function override(config, env) {
       process: "process/browser",
       Buffer: ["buffer", "Buffer"],
     }),
+  ];
+
+  config.ignoreWarnings = [
+    // Ignore warnings raised by source-map-loader.
+    // some third party packages may ship miss-configured sourcemaps, that interrupts the build
+    // See: https://github.com/facebook/create-react-app/discussions/11278#discussioncomment-1780169
+    /**
+     *
+     * @param {import('webpack').WebpackError} warning
+     * @returns {boolean}
+     */
+    function ignoreSourcemapsloaderWarnings(warning) {
+      return (
+        warning.module &&
+        warning.module.resource.includes("node_modules") &&
+        warning.details &&
+        warning.details.includes("source-map-loader")
+      );
+    },
   ];
 
   return config;
